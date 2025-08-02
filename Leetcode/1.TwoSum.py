bfc4992550e4af062ee496e057cc03790a9cd37e
Priyadarshi Kumar <priyadarshi1505@gmail.com>
2025-08-02 12:45:52 +0000

    Add twoSum function to find indices of two numbers that add up to a target

---
+++ Leetcode/1.TwoSum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}
+        for index, num in enumerate(nums):
+            difference = target - num
+            if difference in num_to_index:
+                return [num_to_index[difference], index]
+            num_to_index[num] = index
