93ce3a2bab5e45c350f50fdafa89541f30d1c2fa
iPriyadarshi <priyadarshi1505@gmail.com>
2025-08-20 20:59:03 +0530

    Implement LinkedList class and getCount function to count nodes in a linked list

---
+++ GeekForGeeks/Find Length of Linked List.py
@@ -0,0 +1,24 @@
+# https://www.geeksforgeeks.org/problems/count-nodes-of-linked-list/0
+
+"""
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+"""
+
+
+class Solution:
+    # Function to count nodes of a linked list.
+    # Time Complexity: O(n) where n is number of nodes
+    # Space Complexity: O(1) since we only use a single variable length
+    def getCount(self, head):
+        # code here
+        length = 0
+        currNode = head
+        while currNode:
+            length += 1
+            currNode = currNode.next
+        return length
