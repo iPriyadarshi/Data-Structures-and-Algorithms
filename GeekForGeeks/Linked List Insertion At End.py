f382acd403ec98ac77dd6027c3dc4e7723b64f73
iPriyadarshi <priyadarshi1505@gmail.com>
2025-08-20 21:01:18 +0530

    Implement insertAtEnd function to add a new node at the end of a linked list

---
+++ GeekForGeeks/Linked List Insertion At End.py
@@ -0,0 +1,31 @@
+# https://www.geeksforgeeks.org/problems/linked-list-insertion-1587115620/0
+
+"""
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+"""
+
+
+class Solution:
+    """
+    Time Complexity: O(n) where n is the length of the linked list
+    Space Complexity: O(1) as only constant extra space is used
+    """
+
+    def insertAtEnd(self, head, x):
+        new_node = Node(x)
+
+        # If the list is empty, the new node becomes the head
+        if head is None:
+            return new_node
+
+        # Traverse to the end of the list
+        current = head
+        while current.next:
+            current = current.next
+
+        # Link the new node at the end
+        current.next = new_node
+        return head
