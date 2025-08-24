cbe3b023344bd5e15c8eb09e1c947418092571d5
iPriyadarshi <priyadarshi1505@gmail.com>
2025-08-24 11:29:12 +0530

    Refactor intersection methods to improve readability and efficiency

--- Leetcode/350. Intersection of Two Arrays II.java
+++ Leetcode/350. Intersection of Two Arrays II.java
@@ -1,35 +1,106 @@
-class Solution {
-    /*
-     * Time Complexity: O(n + m) where n and m are lengths of nums1 and nums2
-     * Space Complexity: O(1) since frequency array has fixed size 1001
-     */
-    public int[] intersect(int[] nums1, int[] nums2) {
-        // Frequency array for nums1
-        int[] freq = new int[1001]; // Frequency array for values 0 to 1000
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-        // Count frequencies in nums1
+// Approach-1 (Using frequency array)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        /*
+         * Time Complexity: O(n + m) where n and m are lengths of nums1 and nums2
+         * Space Complexity: O(1) since frequency array has fixed size 1001
+         */
+        int[] freq = new int[1001];
+
         for (int num : nums1) {
             freq[num]++;
         }
 
-        // Build result using nums2
-        int[] temp = new int[Math.min(nums1.length, nums2.length)];
-        int index = 0;
+        ArrayList<Integer> result = new ArrayList<>();
 
         for (int num : nums2) {
             if (freq[num] > 0) {
-                temp[index] = num;
-                index++;
+                result.add(num);
                 freq[num]--;
             }
         }
 
-        // Copy result to final array of correct size
-        int[] result = new int[index];
-        for (int i = 0; i < index; i++) {
-            result[i] = temp[i];
+        int[] arr = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            arr[i] = result.get(i);
         }
 
-        return result;
+        return arr;
+    }
+}
+
+// Approach-2 (Using sorting and 2-Pointers)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        /*
+         * Time Complexity: O(nlogn + mlogm) where n and m are lengths of nums1 and
+         * nums2 (due to sorting)
+         * Space Complexity: O(1) if we don't count the space used for output array
+         */
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        int[] arr = new int[result.size()];
+        for (int k = 0; k < result.size(); k++) {
+            arr[k] = result.get(k);
+        }
+
+        return arr;
+    }
+}
+
+// Approach-3 (Using map)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        /*
+         * Time Complexity: O(n + m) where n and m are lengths of nums1 and nums2
+         * Space Complexity: O(min(n, m)) to store the HashMap
+         */
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] arr = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            arr[i] = result.get(i);
+        }
+
+        return arr;
     }
 }
