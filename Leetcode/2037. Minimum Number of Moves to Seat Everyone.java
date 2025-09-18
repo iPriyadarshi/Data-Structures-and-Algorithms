a74ed2dc40ae2d2f320632c7fd947002ca5b7081
iPriyadarshi <priyadarshi1505@gmail.com>
2025-09-18 19:31:43 +0530

    Add solution to calculate minimum number of moves to seat everyone

---
+++ Leetcode/2037. Minimum Number of Moves to Seat Everyone.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+// T.C: O(n log n) due to sorting
+// S.C: O(1)
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int moves = 0;
+        for (int i = 0; i < seats.length; i++) {
+            moves += Math.abs(seats[i] - students[i]);
+        }
+        return moves;
+
+    }
+}
