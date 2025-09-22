8c2f07a1b73e7d9d69b90128048bec10f6340df3
iPriyadarshi <priyadarshi1505@gmail.com>
2025-09-22 23:02:47 +0530

    Add solution to count the number of arithmetic slices in an array

---
+++ Leetcode/413. Arithmetic Slices.java
@@ -0,0 +1,23 @@
+// T.C: O(n)
+// S.C: O(1)
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return 0;
+
+        int totalSlices = 0;
+        int currentSlices = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                currentSlices++;
+                totalSlices += currentSlices;
+            } else {
+                currentSlices = 0;
+            }
+        }
+
+        return totalSlices;
+    }
+}
