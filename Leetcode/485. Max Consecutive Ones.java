7cc0fcbb6efea500ae92987fc34973f9a0806af5
iPriyadarshi <priyadarshi1505@gmail.com>
2026-01-28 00:00:10 +0530

    Add implementation for Max Consecutive Ones problem with counting logic

---
+++ Leetcode/485. Max Consecutive Ones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int maxCount = 0;
+        int count = 0;
+        for(int i =0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+            } else{
+                maxCount = Math.max(maxCount,count);
+                count = 0;
+            }
+        }
+        return Math.max(maxCount,count);
+    }
+}
