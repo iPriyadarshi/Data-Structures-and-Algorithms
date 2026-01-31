3b530deeb764698e75bfc42baf8a6f497530fbc2
iPriyadarshi <priyadarshi1505@gmail.com>
2026-01-31 13:38:50 +0530

    Add binary search approach for finding the smallest letter greater than target

--- Leetcode/744. Find Smallest Letter Greater Than Target.java
+++ Leetcode/744. Find Smallest Letter Greater Than Target.java
@@ -1,4 +1,5 @@
-class Solution {
+// Approach 1: Linear Search
+class Solution1 {
     public char nextGreatestLetter(char[] letters, char target) {
         int index = 0;
         for(char c : letters){
@@ -9,5 +10,25 @@ class Solution {
             }
         }
         return letters[0];
+    }
+}
+
+// Approach 2: Binary Search
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0, r = letters.length;
+
+        // Binary search for the first letter greater than target
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (letters[mid] <= target) {
+                l = mid + 1;   // move right if mid is too small
+            } else {
+                r = mid;       // keep mid as possible answer
+            }
+        }
+
+        // If l == letters.length, wrap around to first element
+        return letters[l % letters.length];
     }
 }
