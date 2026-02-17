b459d1963e8a5ec9f08c7760824768032b179e8a
iPriyadarshi <priyadarshi1505@gmail.com>
2026-02-17 23:23:13 +0530

    Add implementation for Search Insert Position algorithm

---
+++ Leetcode/35. Search Insert Position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int ans = nums.length;
+
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
+
+// insert position = lower bound of target in nums
