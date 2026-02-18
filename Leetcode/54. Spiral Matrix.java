cfa2e32f4f48e3b1311ee11950f69ee1eeed4de5
iPriyadarshi <priyadarshi1505@gmail.com>
2026-02-18 19:47:51 +0530

    Add implementation for second approach of Spiral Matrix algorithm

--- Leetcode/54. Spiral Matrix.java
+++ Leetcode/54. Spiral Matrix.java
@@ -59,3 +59,42 @@ class Solution {
         return result;
     }
 }
+
+// Approach2:
+
+class Solution2 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        int top = 0;
+        int down = matrix.length - 1;
+
+        while (left <= right && top <= down) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= down; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            
+            if (top <= down) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[down][i]);
+                }
+                down--;
+            }
+            if (left <= right) {
+                for (int i = down; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
